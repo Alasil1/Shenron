375a6aa7d09188f21352d070eaa344058adf0a5e
omr-yasser <123978060+omr-yasser@users.noreply.github.com>
2024-11-26 15:16:55 +0200

    Creat user file

--- shenron/user/models.py
+++ shenron/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import authenticate,login
 
-# Create your models here.
+class User(AbstractUser):
+    @classmethod
+    def CreateUser(cls,username,password,email):
+        user=User.objects.create_user(username=username,password=password,email=email)
+        user.save()
+        return user
+    @classmethod
+    def Login(cls,request,username,password):
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return True
+        else:
+            return False
+
---
+++ shenron/user/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import create_user_view,login
+
+urlpatterns = [
+    path("", create_user_view),
+    path("login",login)
+    ]
