7d181494b05919a178d6f98ff56259862d46dc04
Alasil1 <120193939+Alasil1@users.noreply.github.com>
2024-12-21 19:27:49 +0200

    a

--- shenron/user/signal.py
+++ shenron/user/signal.py
@@ -5,6 +5,6 @@ from allauth.socialaccount.models import SocialAccount
 @receiver(user_signed_up)
 def user_signed_up_request(request, user, **kwargs):
     social_login = SocialAccount.objects.filter(user=user)
-    if social_login:
+    if social_login or user.is_staff or user.is_superuser:
         user.activated = True
         user.save()
