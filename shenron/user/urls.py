d038888a1041194f961fcb26c0a8b7bd1216b794
omaryasser3 <s-omar_yasser@zewailcity.edu.eg>
2024-12-21 12:02:03 +0200

    api

--- shenron/user/urls.py
+++ shenron/user/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import create_user_view,login,logout_view,activate,activate_account,request_forget_password,forget_password
-from django.urls import include
-from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path("", create_user_view,name='signup'),
     path('activate/', activate_account, name='activate_account'),
